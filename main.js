6ea7180012e50a61a887a69724ad2258b66883b8
arutonee <arutonee@proton.me>
2023-09-01 17:20:53 -0400

    More clickable elements update

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ const sources = [
     { data: 'res/steam.svg', href: 'https://steamcommunity.com/profiles/76561199436675295/' },
 ];
 
+let special_bg = false;
+
+const names = [
+    'Arutonee',
+    'aru',
+    'waa!!',
+    'Luna',
+];
+
+let current_name = 0;
+
 function main() {
     add_sources();
 }
@@ -19,7 +30,26 @@ function add_sources() {
         const svg = document.createElement('div');
         svg.style.mask = `url("${source.data}")`;
         svg.style.maskSize = 'contain';
+        svg.style.webkitMask = `url("${source.data}")`;
+        svg.style.webkitMaskSize = 'contain';
         el.appendChild(svg);
         source_list_el.appendChild(el);
     }
+}
+
+function change_background() {
+    if (special_bg) {
+        document.body.style.background = 'var(--background-color)';
+        document.body.style.animation = '';
+    } else {
+        document.body.style.background = 'linear-gradient(to right, #5bcefa, #f5a9b8, #ffffff, #f5a9b8, #5bcefa, #f5a9b8, #ffffff, #f5a9b8, #5bcefa)';
+        document.body.style.backgroundSize = '200%';
+        document.body.style.animation = 'background-pan 1s linear infinite';
+    }
+    special_bg ^= true;
+}
+
+function change_name() {
+    current_name = (current_name + 1) % names.length;
+    document.querySelector('#name').innerText = names[current_name];
 }
